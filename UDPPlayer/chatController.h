7aa1f04f06f86bda4d3be3bc0e1ba198b68ab8c4
john-heaven <johnheaven@tom.com>
2016-06-08 10:40:15 +0800

    1,第一版VR播放器的版本

---
+++ UDPPlayer/chatController.h
@@ -0,0 +1,13 @@
+//
+//  chatController.h
+//  UDPPlayer
+//
+//  Created by Mac_jiang on 16/5/3.
+//  Copyright © 2016年 joky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface chatController : UIViewController
+
+@end
